41d0c042cf67e66e7872291785c35e17ddae4f94
nguyenhuyk3 <huykimcuong5@gmail.com>
2025-09-10 14:18:41 +0700

    chore: init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello Tailwind + React + Vite 🚀
+      </h1>
+    </div>
+  )
+}
+
